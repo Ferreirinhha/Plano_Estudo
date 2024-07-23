38782d42a95e99b816b5ed02108f5e7372caa228
Ferreirinhha <danielferreirinham@gmail.com>
2024-07-23 18:46:12 -0300

    Update

---
+++ Aprendizado/Boas_praticas.py
@@ -0,0 +1,52 @@
+# Boas praticas
+
+# Importações
+
+""" 
+1° import bibliotecas padrões
+
+2° import bibliotecas de terceiros
+
+3° import bibliotecas locais
+"""
+
+# Nomes
+
+""" 
+snake_case = 2 # variaveis
+
+def snake_case() # funções
+
+
+class camelCase: # Classes
+
+
+UPPER_CASE = 2 # Constantes 
+"""
+
+# Docstrings
+
+""" 
+def minha_funcao(param1, param2):
+    ""
+    Descrição breve da função.
+
+    Args:
+        param1: O primeiro parâmetro.
+        param2: O segundo parâmetro.
+
+    Returns:
+        O resultado da operação.
+    ""
+    pass
+"""
+
+# Evitar Clever-Codes:
+
+"""
+Clever Codes são codigos muito complexos, dificultando na manutencão, 
+visibilidade e compreensibilidade 
+
+
+Exemplos: list Comprehension, Lambda, Operadores Ternários 
+"""
--- Listas aninhadas/Exercicios/Ex4.py
+++ Listas aninhadas/Exercicios/Ex4.py
@@ -1,2 +1,32 @@
 """ Crie uma função que gere uma matriz identidade de tamanho n, onde n é 
 um número inteiro positivo passado como argumento  """   
+
+def matriz_identidade(tam: int):
+    matriz = list()
+
+    # Criar matriz
+    for i in range(tam):
+        matriz.append([])
+
+    # Gerar Numeros na matriz
+    for i in range(len(matriz)):
+        for j in range(tam):
+            matriz[i].append(0)
+    
+    # Criar a linha diagonal
+    for i in range(len(matriz)):
+        for j in range(len(matriz)):
+            if i == j:
+                matriz[i][j] = 1
+    return matriz
+
+
+
+def matrizidentidade(tam: int):
+    return [[1 if i == j else 0 for j in range(tam)] for i in range(tam)]
+
+
+matriz = matriz_identidade(3)
+
+for i in range(len(matriz)):
+    print(matriz[i])
